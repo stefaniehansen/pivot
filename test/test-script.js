e9fcd34991a567e08a8aa08e24eb710de6c49ac7
Angel Duran <angeldm03@hotmail.com>
2019-07-22 21:56:48 -0700

    Adding special template for return

--- test/test-script.js
+++ test/test-script.js
@@ -1,3 +1,48 @@
-if (n === 3) {
-    console.log('hi')
+import { fromKeyword } from '@sweet-js/helpers' for syntax
+
+syntax para = ctx => {
+  return #`for`;
+};
+
+syntax funcion = ctx => {
+  return #`function`;
+};
+
+syntax mientras = ctx => {
+  return #`while`;
+};
+
+syntax retorna = ctx => {
+    let ident = ctx.next().value;
+    return #`return ${ident}`;
+};
+
+syntax variable = ctx => {
+    let ident = ctx.next().value;
+    return #`var ${ident}`;
+};
+
+// ACTUAL SCRIPT STARTS HERE:
+
+fibonacci(2);
+  
+para(i = 0; i < 5; i++){
+    console.log(i);
+}
+
+funcion fibonacci(num){
+    variable a = 1, b = 0, temp;
+
+    mientras (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+    }
+
+    variable unaFuncion = funcion() {
+        console.log("");
+    }
+
+    retorna unaFuncion;
 }
