79f71caa9fe9dcdd583973c3c628065964432e00
Linsen Wu <linswu@microsoft.com>
2019-07-22 18:19:59 -0700

    Adding sweet stuff. Yum

---
+++ src/sweet-js-test.js
@@ -0,0 +1,10 @@
+import { fromKeyword } from '@sweet-js/helpers' for syntax\
+
+syntax para = ctx => {
+  return #`for`;
+};
+
+para(var i = 0; i < 5; i++) {
+  console.log('yo dawg')
+  var paragraph = 'para'
+}
