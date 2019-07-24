000d80be29e654018ebf8e25fd355d32958f8f31
Angel Duran <angeldm03@hotmail.com>
2019-07-24 00:19:49 -0700

    merging the current content of master in test-script.js
    merging the current content of master in test-script.js

--- test/test-script.js
+++ test/test-script.js
@@ -1,33 +1,52 @@
-import { unwrap, isKeyword, fromIdentifier } from '@sweet-js/helpers' for syntax
-
-syntax para = ctx => {
-    let forExpression = ctx.contextify(ctx.next().value);
-    let parensContent = #``;
-    for (let forItem of forExpression) {
-        let unwrappedItem = unwrap(forItem);
-        if(unwrappedItem.value === "variable"){
-            parensContent = parensContent.concat(#`var`);
-        }
-        else 
-        {
-            parensContent = parensContent.concat(#`${forItem}`);
-        }
-    }
-
-    let forContext = ctx.next().value;
-    let result = #`for (${parensContent}) ${forContext}`;
-    
-    return result;
-};
-
-//The `syntax` keyword is used to create and name new macros.
-syntax variable = ctx => {
-    return #`var`;
-};
-
 // CODE TO PARSE STARTS HERE
 para (variable i = 0; i < 5; i++)
 {
     variable testing = 3;
     console.log("It Works: " + i);
+}
+
+variable miFuncion = funcion(num) {
+    retorna num + 1;
+}
+
+funcion fibonacci(num){
+    variable a = 1, b = 0, temp;
+
+    mientras (num >= 0) {
+        temp = a;
+        a = a + b;
+        b = temp;
+        num--;
+    }
+
+    variable unaFuncion = funcion() {
+        console.log("");
+    }
+
+    retorna unaFuncion;
+}
+
+funcion testIfElse(isItTrue){
+    si(isItTrue) {
+        console.log("It is true, but why?");
+        si(isItTrue){
+            console.log("nested if");
+        }
+        sino si(isItTrue){
+            console.log("wattt");
+        }
+        sino {
+            console.log("wat");
+        }
+    }
+    sino si (isItTrue == false) {
+        console.log("More ifs???");
+        console.log("anotherconsole");
+    }
+    sino si (false) {
+        console.log("How did it get here? not good with computers");
+    }
+    sino {
+        console.log("papanada");
+    }
 }
